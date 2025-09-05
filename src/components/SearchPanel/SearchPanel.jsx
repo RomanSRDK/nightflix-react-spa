57d9eab439f757296663008ea5924d4f68febec6
Roman Serdiuk <roman.srdk21@gmail.com>
2025-09-05 11:55:28 -0400

    enable movie search on Search button click

--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -37,15 +37,79 @@ function SearchPanel() {
 
   return (
     <>
-      <input
-        type="text"
-        placeholder="Find your movie for tonight"
-        value={filmName}
-        onChange={changeSearchQuery}
-        className={styles.searchInput}
-      />
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          placeholder="Find your movie for tonight"
+          value={filmName}
+          onChange={changeSearchQuery}
+          className={styles.searchInput}
+        />
+      </div>
     </>
   );
 }
 
 export default SearchPanel;
+
+// function SearchPanel() {
+//   const dispatch = useDispatch();
+//   const [searchParams, setSearchParams] = useSearchParams();
+
+//   const query = searchParams.get("name") ?? "";
+//   const page = parseInt(searchParams.get("page")) || 1;
+
+//   const [filmName, setFilmName] = useState(query);
+
+//   const changeSearchQuery = (evt) => {
+//     setFilmName(evt.target.value);
+//   };
+
+//   const handleSearch = () => {
+//     const nextSearchParams = new URLSearchParams(searchParams);
+
+//     if (filmName.trim()) {
+//       nextSearchParams.set("name", filmName.trim());
+//       nextSearchParams.set("page", 1);
+//       setSearchParams(nextSearchParams);
+
+//       dispatch(getMoviesByName({ query, page }));
+//       setFilmName("");
+//     } else {
+//       nextSearchParams.delete("name");
+//       nextSearchParams.delete("page");
+
+//       dispatch(clearFoundMovies());
+//     }
+//   };
+
+//   const handleKeyPress = (evt) => {
+//     if (evt.key === "Enter") {
+//       handleSearch();
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (query && page > 1) {
+//       dispatch(getMoviesByName({ query: query.trim(), page }));
+//     }
+//   }, [page, query, dispatch]);
+
+//   return (
+//     <div className={styles.searchContainer}>
+//       <input
+//         type="text"
+//         placeholder="Find your movie for tonight"
+//         value={filmName}
+//         onChange={changeSearchQuery}
+//         onKeyDown={handleKeyPress}
+//         className={styles.searchInput}
+//       />
+//       <button onClick={handleSearch} className={styles.searchButton}>
+//         Search
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default SearchPanel;
